f29a6e829a420f89207cf0cf40efa09b54fb4264
AqsaIftikhar15 <aqsaiftikhar2021@gmail.com>
2025-08-08 14:38:38 +0500

    modifies main.py and requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from configuration import config
 from agents_needed.career_agent import CareerAgent
 from chainlit import on_message , Message
 from agents import Runner
+import os
+import chainlit as cl
 
 @on_message
 async def on_message(msg):
@@ -12,3 +14,7 @@ async def on_message(msg):
         max_turns=150
     )
     await Message(content=result.final_output).send()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    cl.run(port=port, host="0.0.0.0")
